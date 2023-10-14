783999adec41e3596d95458dce463d48f82781d0
Rahat Chowdhury <rczisan@gmail.com>
2023-10-15 00:45:14 +0600

    added functionality of search button and details button along with styles for responsiveness

--- main.js
+++ main.js
@@ -3,8 +3,12 @@
 const searchField = document.getElementById("search-field");
 const searchBtn = document.getElementById("search-btn");
 const resultDiv = document.getElementById("result-container");
+const loaderDiv = document.getElementById("loader");
+
+
 
 searchBtn.addEventListener('click', () => {
+  loaderDiv.style.display = "block";
   const url = `https://pokeapi.co/api/v2/pokemon/${searchField.value}`;
   fetch(url)
     .then(res => res.json())
@@ -12,20 +16,24 @@ searchBtn.addEventListener('click', () => {
   });
 
   const displayPokemon = (pokemon) => {
+    loaderDiv.style.display = "none";
     resultDiv.style.display = "block";
+    
     resultDiv.innerHTML =
-      `<h4>A wild ${pokemon.name} appeared!</h4>
+    `<h4>A wild ${pokemon.name} appeared!</h4>
     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" />
     <button id="details-btn">Details</button>
-  `;
+    `;
+
     const detailsBtn = document.getElementById("details-btn");
+    
     detailsBtn.addEventListener('click', () => {
       console.log("You entered " + pokemon.name);
-      searchField.innerText = "";
       showDetails(pokemon);
+      detailsBtn.style.display = "none";
     })
   };
-
+  
   const showDetails = (pokemon) => {
     const itemTitles = document.createElement("h4");
     itemTitles.innerText = "Held Items: ";
@@ -33,7 +41,7 @@ searchBtn.addEventListener('click', () => {
 
     pokemon.held_items.forEach(item => {
       const itemNames = document.createElement("p");
-      itemNames.innerText = `${item.item.name}`;
+      itemNames.innerText = `${item && item.item.name || "No item"}`;
       resultDiv.appendChild(itemNames);
     });
 
@@ -46,4 +54,5 @@ searchBtn.addEventListener('click', () => {
       typeNames.innerText = `${type.type.name}`;
       resultDiv.appendChild(typeNames);
     });
+    searchField.value = "";
   }
