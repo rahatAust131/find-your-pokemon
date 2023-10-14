4724e9efd9ad6e4ac509b87688facffc131bcac1
Rahat Chowdhury <rczisan@gmail.com>
2023-10-15 01:31:08 +0600

    added some error handling for non-existing pokemon search

--- main.js
+++ main.js
@@ -10,49 +10,76 @@ const loaderDiv = document.getElementById("loader");
 searchBtn.addEventListener('click', () => {
   loaderDiv.style.display = "block";
   const url = `https://pokeapi.co/api/v2/pokemon/${searchField.value}`;
-  fetch(url)
-    .then(res => res.json())
-    .then(result => displayPokemon(result))
-  });
 
-  const displayPokemon = (pokemon) => {
-    loaderDiv.style.display = "none";
-    resultDiv.style.display = "block";
+  async function makeRequest() {
+    try {
+      const response = await fetch(url);
+      console.log('status code: ', response.status);
+  
+      if (!response.ok) {
+        console.log(response);
+        loaderDiv.style.display = "none";
+        alert(`No pokemon found named "${searchField.value}" in the Pokedex!`);
+        searchField.value = "";
+        throw new Error(`Error! status: ${response.status}`);
+      }
+  
+      const result = await response.json();
+      displayPokemon(result);
+      
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  
+  makeRequest();
     
+  
+});
+
+const displayPokemon = (pokemon) => {
+  console.log(pokemon);
+  loaderDiv.style.display = "none";
+  resultDiv.style.display = "block";
+
+  if (pokemon) {
     resultDiv.innerHTML =
-    `<h4>A wild ${pokemon.name} appeared!</h4>
+      `<h4>A wild ${pokemon.name} appeared!</h4>
     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" />
     <button id="details-btn">Details</button>
     `;
 
     const detailsBtn = document.getElementById("details-btn");
-    
+
     detailsBtn.addEventListener('click', () => {
       console.log("You entered " + pokemon.name);
       showDetails(pokemon);
       detailsBtn.style.display = "none";
     })
-  };
-  
-  const showDetails = (pokemon) => {
-    const itemTitles = document.createElement("h4");
-    itemTitles.innerText = "Held Items: ";
-    resultDiv.appendChild(itemTitles);
-
-    pokemon.held_items.forEach(item => {
-      const itemNames = document.createElement("p");
-      itemNames.innerText = `${item && item.item.name || "No item"}`;
-      resultDiv.appendChild(itemNames);
-    });
-
-    const typeTitles = document.createElement("h4");
-    typeTitles.innerText = "Types: ";
-    resultDiv.appendChild(typeTitles);
-
-    pokemon.types.forEach(type => {
-      const typeNames = document.createElement("p");
-      typeNames.innerText = `${type.type.name}`;
-      resultDiv.appendChild(typeNames);
-    });
-    searchField.value = "";
+  } else {
+    alert("No Pokemon found with this name in the database!")
   }
+};
+
+const showDetails = (pokemon) => {
+  const itemTitles = document.createElement("h4");
+  itemTitles.innerText = "Held Items: ";
+  resultDiv.appendChild(itemTitles);
+
+  pokemon.held_items.forEach(item => {
+    const itemNames = document.createElement("p");
+    itemNames.innerText = `${item && item.item.name || "No item"}`;
+    resultDiv.appendChild(itemNames);
+  });
+
+  const typeTitles = document.createElement("h4");
+  typeTitles.innerText = "Types: ";
+  resultDiv.appendChild(typeTitles);
+
+  pokemon.types.forEach(type => {
+    const typeNames = document.createElement("p");
+    typeNames.innerText = `${type.type.name}`;
+    resultDiv.appendChild(typeNames);
+  });
+  searchField.value = "";
+}
